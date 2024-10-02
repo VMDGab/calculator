29b80f76d310ff4c171a85b004551b9714bafea3
Gab <gvdantas1@stefanini.com>
2024-10-02 14:46:50 -0300

    Done

--- projeto01/Program.cs
+++ projeto01/Program.cs
@@ -3,72 +3,88 @@
     static void Main(string[] args)
     {
         bool novaOperacao = true;
+        double resultado = 0.0;
 
         while (novaOperacao)
         {
-            Console.WriteLine("Informe o tipo de operação (Soma, subtração, divisão ou multiplicação)");
+            Console.WriteLine(
+                "Informe o tipo de operação" +
+                "\n1 - Soma" +
+                "\n2 - subtração " +
+                "\n3 - multiplicação" +
+                "\n4 - divisão"
+                );
+
             String operacao = Console.ReadLine();
 
             if (
-                operacao.ToLower() != "soma" &&
-                operacao.ToLower() != "subtração" &&
-                operacao.ToLower() != "divisão" &&
-                operacao.ToLower() != "multiplicação"
+                operacao != "1" &&
+                operacao != "2" &&
+                operacao != "3" &&
+                operacao != "4"
                 )
             {
                 Console.WriteLine("Erro! Informe uma operação válida");
-                return;
+                continue;
             }
 
+            try
+            {
             Console.WriteLine("Informe o primeiro valor");
             double valor1 = double.Parse(Console.ReadLine());
-
+                      
             Console.WriteLine("Informe o segundo valor");
             double valor2 = double.Parse(Console.ReadLine());
-
-            if (operacao.ToLower() == "soma")
-            {
-                Console.WriteLine("\nO resultado da soma é:");
-                double resultado = Soma.calcular(valor1, valor2);
-                Console.WriteLine(resultado);
+            
+            Console.WriteLine(
+                calcularResultado(valor1, valor2, operacao)
+                ); 
             }
-            else if (operacao.ToLower() == "subtração")
-            {
-                Console.WriteLine("\nO resultado da subtração é:");
-                double resultado = Subtracao.calcular(valor1, valor2);
-                Console.WriteLine(resultado);
-            }
-            else if (operacao.ToLower() == "multiplicação")
-            {
-                Console.WriteLine("\nO resultado da multiplicação é:");
-                double resultado = Multiplicacao.calcular(valor1, valor2);
-                Console.WriteLine(resultado);
-            }
-            else if (operacao.ToLower() == "divisão")
-            {
-                Console.WriteLine("\nO resultado da divisão é:");
-                double resultado = Divisao.calcular(valor1, valor2);
-                Console.WriteLine(resultado);
-            }
-            else
-            {
-                Console.WriteLine("Erro! Não foi possivel realizar a operação");
+            catch { 
+                Console.WriteLine("Insira valores válidos");
             }
 
-            Console.WriteLine("Deseja realizar uma nova operação?");
+            
+
+            Console.WriteLine("Deseja realizar uma nova operação? S/N");
             String resposta = Console.ReadLine();
 
-            if (resposta.ToLower() == "não")
+            if (resposta.ToLower() == "n")
             {
-                Console.WriteLine("Saindo...");
                 novaOperacao = false;
             }
-            else
+            else if(resposta.ToLower() == "s")
             {
                 novaOperacao = true;
             }
-
+            else
+            {
+                Console.WriteLine("Resposta inválida, fechando programa...");
+                novaOperacao = false;
+            }
         }
 
+    }
+
+    static string calcularResultado(double valor1, double valor2, string operacao)
+    {
+        double resultado = 0.0;
+
+        switch (operacao){
+            case "1":
+               resultado = Soma.calcular(valor1, valor2);
+                break;
+            case "2":
+                resultado = Subtracao.calcular(valor1, valor2);
+                break;
+            case "3":
+                resultado = Multiplicacao.calcular(valor1, valor2);
+                break;
+            case "4":
+                resultado = Divisao.calcular(valor1, valor2);
+                break;
+        }
+
+        return $"O resultado da operação é : {resultado}";
     }
 }
